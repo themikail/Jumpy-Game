4d18ddb979ced3ea31f9da264d0ed08e33a9ffdf
Mikail Gökce <66737755+themikail@users.noreply.github.com>
2021-05-04 13:46:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var character = document.getElementById('character');
+var block = document.getElementById('block');
+var counter = 0;
+
+// var jumpy = new Audio();
+// jumpy.src = 'jumpy.mp3';
+
+function jump() {
+	// jumpy.play();
+	if (character.classList == 'animate') {
+		return;
+	}
+	character.classList.add('animate');
+	setTimeout(function () {
+		character.classList.remove('animate');
+	}, 300);
+}
+var checkDead = setInterval(function () {
+	let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+	let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+	if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
+		block.style.animation = 'none';
+		alert('Game Over! Score: ' + Math.floor(counter / 100));
+		document.getElementById('gameScore').innerHTML = 'Last Score: ' + Math.floor(counter / 100);
+		counter = 0;
+		block.style.animation = 'block 1s infinite linear';
+	} else {
+		counter++;
+		document.getElementById('scoreSpan').innerHTML = Math.floor(counter / 100);
+	}
+}, 10);
